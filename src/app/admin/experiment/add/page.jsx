d5c0096dcd23ee1f480b304fce5e980e7c2ff22e
AdrianoII <adrianocorbelinoii@gmail.com>
2025-09-02 02:33:48 -0400

    Prepare front end to support per block relation

--- src/app/admin/experiment/add/page.jsx
+++ src/app/admin/experiment/add/page.jsx
@@ -13,12 +13,8 @@ import { useForm, Controller, useController } from "react-hook-form"
 import { ospfGetPools, ospfGetStimuli } from "../../../_utilities/ospf"
 import { useAsync } from "react-use"
 import { createExperiment } from "../../../_utilities/ExperimentService"
-import { LuPlus, LuFlaskConical } from 'react-icons/lu'
+import { LuPlus, LuFlaskConical, LuTrash2 } from 'react-icons/lu'
 import { useRouter } from 'next/navigation';
-
-const items = [
-  { block_idx: 1, type: "rel", n_dist: 2 },
-]
 
 const block_types = createListCollection({
   items: [
@@ -41,11 +37,12 @@ export default function Experiment() {
     handleSubmit,
     watch,
     formState: { errors },
-    control
+    control,
+    setValue
   } = useForm();
 
-  const [nextBlock, setNextBlock] = useState(1);
   const [blocks, setBlocks] = useState([]);
+
 
   const feedback = useController({
     control: control,
@@ -72,7 +69,12 @@ export default function Experiment() {
     name: "related",
   });
 
+  const [selBlock, setSelBlock] = useState(null);
   const [relation, setRelation] = useState({});
+
+  useEffect(() => {
+    setRelation({});
+  }, [poolId]);
 
   const onSubmit = async (data) => {
     console.log("Creating new experiment");
@@ -106,37 +108,70 @@ export default function Experiment() {
   }, [pools.value]);
 
   const addBlock = (e) => {
-    console.log("Adding new");
-    console.log(blocks);
-    // let block =  { block_idx: nextBlock, type: "rel", n_dist: 2 };
-    let block = { block_idx: nextBlock, n_dist: 1, n_trials: 1 };
-    console.log([...blocks, block]);
-    setNextBlock(nextBlock + 1);
-    setBlocks([...blocks, block]);
+    let block = { id: nextBlock, n_dist: 0, n_trials: 1, feedback: false, metrics: false, rel: {} };
+    const new_blocks = [...blocks, block];
+    setSelBlock(new_blocks.length - 1);
+    setBlocks(new_blocks);
+    setValue("stId", undefined);
+    setValue("related", undefined);
   };
 
   const updateBlockNDist = (block_idx, n_dist) => {
-    let new_blocks = blocks.map((block) =>
-      block.block_idx === block_idx ? { ...block, n_dist: n_dist } : block
+    let new_blocks = blocks.map((block, idx) =>
+      idx === block_idx ? { ...block, n_dist: n_dist } : block
     )
-    console.log(new_blocks);
     setBlocks(new_blocks);
   };
 
   const updateBlockNTrials = (block_idx, n_trials) => {
-    let new_blocks = blocks.map((block) =>
-      block.block_idx === block_idx ? { ...block, n_trials: n_trials } : block
+    let new_blocks = blocks.map((block, idx) =>
+      idx === block_idx ? { ...block, n_trials: n_trials } : block
     )
-    console.log(new_blocks);
     setBlocks(new_blocks);
   };
 
   const updateBlockType = (block_idx, type) => {
-    let new_blocks = blocks.map((block) =>
-      block.block_idx === block_idx ? { ...block, type: type } : block
+    let new_blocks = blocks.map((block, idx) =>
+      idx === block_idx ? { ...block, type: type } : block
     )
-    console.log(new_blocks);
     setBlocks(new_blocks);
+  };
+
+  const updateBlockRel = (block_idx, rel) => {
+    let new_blocks = blocks.map((block, idx) =>
+      idx === block_idx ? { ...block, rel: rel } : block
+    )
+    setBlocks(new_blocks);
+  };
+
+  const updateBlockFeedback = (block_idx, feedback) => {
+    let new_blocks = blocks.map((block, idx) =>
+      idx === block_idx ? { ...block, feedback: feedback } : block
+    )
+    setBlocks(new_blocks);
+  };
+
+  const updateBlockMetric = (block_idx, metrics) => {
+    let new_blocks = blocks.map((block, idx) =>
+      idx === block_idx ? { ...block, metrics: metrics } : block
+    )
+    setBlocks(new_blocks);
+  };
+
+  const handleTableClick = (block_idx) => {
+    updateBlockRel(selBlock, relation);
+    setSelBlock(block_idx);
+    setRelation(blocks[block_idx].rel);
+    setValue("stId", undefined);
+    setValue("related", undefined);
+  };
+
+  const deleteBlock = (block_idx) => {
+    const new_blocks = [...blocks.slice(0, block_idx), ...blocks.slice(block_idx + 1)];
+    setBlocks(new_blocks);
+    setSelBlock(null);
+    setValue("stId", undefined);
+    setValue("related", undefined);
   };
 
   return <Container>
@@ -216,126 +251,171 @@ export default function Experiment() {
           />
         </HStack>
         <Separator size="lg" />
-        <Heading size="4xl">Define the stimuli relation</Heading>
-        <Flex mt={4} direction="row" justify="center" gap="4">
-          <Fieldset.Root>
-            <Fieldset.Legend>Select a stimulus</Fieldset.Legend>
-            <Controller
-              name="stId"
-              control={control}
-              render={({ field }) => (
-                <RadioGroup.Root
-                  name={field.name}
-                  value={field.value}
-                  onValueChange={({ value }) => {
-                    // related.field.value = relation[value]
-                    related.field.value = relation[value] !== undefined ? relation[value] : []
-                    field.onChange(value)
-                  }}
-                >
-                  <HStack gap="6">
-                    {poolId.field.value && !stimuli.loading && !stimuli.error && stimuli.value.map((item) => (
-                      <RadioGroup.Item key={item} value={item}>
-                        <RadioGroup.ItemHiddenInput onBlur={field.onBlur} />
-                        <RadioGroup.ItemIndicator />
-                        <RadioGroup.ItemText>{item}</RadioGroup.ItemText>
-                      </RadioGroup.Item>
-                    ))}
-                  </HStack>
-                </RadioGroup.Root>
-              )} />
-          </Fieldset.Root>
-          <Fieldset.Root>
-            <Fieldset.Legend>Select related stimuli</Fieldset.Legend>
-            <CheckboxGroup width="50%"
-              value={related.field.value}
-              onValueChange={(e) => {
-                let old_rel = relation;
-                old_rel[stId.field.value] = e;
-                setRelation(old_rel)
-                return related.field.onChange(e)
-              }}
-              name={related.field.name}
-            >
-              <Fieldset.Content>
-                {poolId.field.value && !stimuli.loading && !stimuli.error && stimuli.value.map((item) => (
-                  <Checkbox.Root key={item} value={item}>
-                    <Checkbox.HiddenInput />
-                    <Checkbox.Control />
-                    <Checkbox.Label>{item}</Checkbox.Label>
-                  </Checkbox.Root>
-                ))}
-              </Fieldset.Content>
-            </CheckboxGroup>
-          </Fieldset.Root>
-        </Flex>
-        <Separator size="lg" />
-        <Stack gap="10">
-          <Heading size="4xl">Blocks <Button colorPalette="green" onClick={addBlock}> <LuPlus /> Add a new block</Button></Heading> 
-          <Table.Root size="sm" variant="outline" interactive showColumnBorder>
-            <Table.Header>
-              <Table.Row>
-                <Table.ColumnHeader htmlWidth="10%">Block #</Table.ColumnHeader>
-                <Table.ColumnHeader htmlWidth="25%"># of trials</Table.ColumnHeader>
-                <Table.ColumnHeader htmlWidth="25%"># of distractors</Table.ColumnHeader>
-                <Table.ColumnHeader htmlWidth="40%">Type</Table.ColumnHeader>
-              </Table.Row>
-            </Table.Header>
-            <Table.Body>
-              {blocks.map((block) => (
-                <Table.Row key={block.block_idx}>
-                  <Table.Cell>{block.block_idx}</Table.Cell>
-                  <Table.Cell>
-                    <NumberInput.Root defaultValue={1} min={1} onValueChange={(e) => updateBlockNTrials(block.block_idx, parseInt(e.value))}>
-                      <NumberInput.Control>
-                        <NumberInput.IncrementTrigger />
-                        <NumberInput.DecrementTrigger />
-                      </NumberInput.Control>
-                      <NumberInput.Scrubber />
-                      <NumberInput.Input />
-                    </NumberInput.Root>
-                  </Table.Cell>
-                  <Table.Cell>
-                    <NumberInput.Root defaultValue={1} min={1} onValueChange={(e) => updateBlockNDist(block.block_idx, parseInt(e.value))}>
-                      <NumberInput.Control>
-                        <NumberInput.IncrementTrigger />
-                        <NumberInput.DecrementTrigger />
-                      </NumberInput.Control>
-                      <NumberInput.Scrubber />
-                      <NumberInput.Input />
-                    </NumberInput.Root>
-                  </Table.Cell>
-                  <Table.Cell textAlign="end">
-                    <Select.Root required collection={block_types} size="sm" onValueChange={(e) => updateBlockType(block.block_idx, e.value[0])}>
-                      <Select.HiddenSelect />
-                      <Select.Control>
-                        <Select.Trigger>
-                          <Select.ValueText placeholder="Select the block type" />
-                        </Select.Trigger>
-                        <Select.IndicatorGroup>
-                          <Select.Indicator />
-                        </Select.IndicatorGroup>
-                      </Select.Control>
-                      <Portal>
-                        <Select.Positioner>
-                          <Select.Content>
-                            {block_types.items.map((type) => (
-                              <Select.Item item={type} key={type.value}>
-                                {type.label}
-                                <Select.ItemIndicator />
-                              </Select.Item>
-                            ))}
-                          </Select.Content>
-                        </Select.Positioner>
-                      </Portal>
-                    </Select.Root>
-                  </Table.Cell>
+        {poolId.field.value === undefined && <Heading size="4xl">Select a stimuli pool!</Heading>}
+        {poolId.field.value !== undefined &&
+          <Stack gap="10">
+            <Heading size="4xl">Blocks <Button colorPalette="green" onClick={addBlock}> <LuPlus /> Add a new block</Button></Heading>
+            <Table.Root size="sm" variant="outline" interactive showColumnBorder>
+              <Table.Header>
+                <Table.Row>
+                  <Table.ColumnHeader textAlign="center" htmlWidth="5%"></Table.ColumnHeader>
+                  <Table.ColumnHeader textAlign="center" htmlWidth="5%">Block</Table.ColumnHeader>
+                  <Table.ColumnHeader textAlign="center" htmlWidth="20%"># of trials</Table.ColumnHeader>
+                  <Table.ColumnHeader textAlign="center" htmlWidth="20%"># of distractors</Table.ColumnHeader>
+                  <Table.ColumnHeader textAlign="center" htmlWidth="20%">Type</Table.ColumnHeader>
+                  <Table.ColumnHeader textAlign="center" htmlWidth="10%">Feedback</Table.ColumnHeader>
+                  <Table.ColumnHeader textAlign="center" htmlWidth="10%">Metrics</Table.ColumnHeader>
+                  <Table.ColumnHeader textAlign="center" htmlWidth="10%"></Table.ColumnHeader>
                 </Table.Row>
-              ))}
-            </Table.Body>
-          </Table.Root>
-        </Stack>
-
+              </Table.Header>
+              <Table.Body>
+                {blocks.map((block, block_idx) => (
+                  // <Table.Row key={block.id} onClick={() => handleTableClick(block_idx)}  data-selected={selBlock === block_idx ? "" : undefined}>
+                  <Table.Row key={block.id} data-selected={selBlock === block_idx ? "" : undefined}>
+                    <Table.Cell>
+                      <Center>
+                        <RadioGroup.Root value={selBlock}>
+                          <RadioGroup.Item key={block_idx} value={block_idx} onClick={() => handleTableClick(block_idx)}>
+                            <RadioGroup.ItemHiddenInput />
+                            <RadioGroup.ItemIndicator />
+                          </RadioGroup.Item>
+                        </RadioGroup.Root>
+                      </Center>
+                    </Table.Cell>
+                    <Table.Cell textAlign="center">{block_idx + 1}</Table.Cell>
+                    <Table.Cell>
+                      <NumberInput.Root defaultValue={1} min={1} onValueChange={(e) => updateBlockNTrials(block_idx, parseInt(e.value))}>
+                        <NumberInput.Control>
+                          <NumberInput.IncrementTrigger />
+                          <NumberInput.DecrementTrigger />
+                        </NumberInput.Control>
+                        <NumberInput.Scrubber />
+                        <NumberInput.Input />
+                      </NumberInput.Root>
+                    </Table.Cell>
+                    <Table.Cell>
+                      <NumberInput.Root defaultValue={1} min={0} max={stimuli.value.length - 1} onValueChange={(e) => updateBlockNDist(block_idx, parseInt(e.value))}>
+                        <NumberInput.Control>
+                          <NumberInput.IncrementTrigger />
+                          <NumberInput.DecrementTrigger />
+                        </NumberInput.Control>
+                        <NumberInput.Scrubber />
+                        <NumberInput.Input />
+                      </NumberInput.Root>
+                    </Table.Cell>
+                    <Table.Cell textAlign="end">
+                      <Select.Root required collection={block_types} size="sm" onValueChange={(e) => updateBlockType(block_idx, e.value[0])}>
+                        <Select.HiddenSelect />
+                        <Select.Control>
+                          <Select.Trigger>
+                            <Select.ValueText placeholder="Select the block type" />
+                          </Select.Trigger>
+                          <Select.IndicatorGroup>
+                            <Select.Indicator />
+                          </Select.IndicatorGroup>
+                        </Select.Control>
+                        <Portal>
+                          <Select.Positioner>
+                            <Select.Content>
+                              {block_types.items.map((type) => (
+                                <Select.Item item={type} key={type.value}>
+                                  {type.label}
+                                  <Select.ItemIndicator />
+                                </Select.Item>
+                              ))}
+                            </Select.Content>
+                          </Select.Positioner>
+                        </Portal>
+                      </Select.Root>
+                    </Table.Cell>
+                    <Table.Cell textAlign="center">
+                      <Checkbox.Root
+                        checked={block.feedback}
+                        onCheckedChange={(e) => updateBlockFeedback(block_idx, !!e.checked)}
+                      >
+                        <Checkbox.HiddenInput />
+                        <Checkbox.Control />
+                      </Checkbox.Root>
+                    </Table.Cell>
+                    <Table.Cell textAlign="center">
+                      <Checkbox.Root
+                        checked={block.metrics}
+                        onCheckedChange={(e) => updateBlockMetric(block_idx, !!e.checked)}
+                      >
+                        <Checkbox.HiddenInput />
+                        <Checkbox.Control />
+                      </Checkbox.Root>
+                    </Table.Cell>
+                    <Table.Cell textAlign="center">
+                      <Button colorPalette="red" size="sm" onClick={(e) => deleteBlock(block_idx)}> <LuTrash2 /> </Button>
+                    </Table.Cell>
+                  </Table.Row>
+                ))}
+              </Table.Body>
+            </Table.Root>
+          </Stack>
+        }
+        <Separator size="lg" />
+        {/* {selBlock === null && <Heading size="4xl">Select a block to define its relation</Heading>} */}
+        {selBlock !== null && <Container>
+          <Heading size="4xl">Define the train relation for block #{selBlock + 1}</Heading>
+          {poolId.field.value === undefined && <Heading size="4xl">Select a pool to define the train relation for block #{selBlock + 1}</Heading>}
+          {poolId.field.value !== undefined &&
+            <Flex mt={4} direction="row" justify="center" gap="4">
+              <Fieldset.Root>
+                <Fieldset.Legend>Select a stimulus</Fieldset.Legend>
+                <Controller
+                  name="stId"
+                  control={control}
+                  render={({ field }) => (
+                    <RadioGroup.Root
+                      name={field.name}
+                      value={field.value}
+                      onValueChange={({ value }) => {
+                        // related.field.value = relation[value]
+                        related.field.value = relation[value] !== undefined ? relation[value] : []
+                        field.onChange(value)
+                      }}
+                    >
+                      <HStack gap="6">
+                        {poolId.field.value && !stimuli.loading && !stimuli.error && stimuli.value.map((item) => (
+                          <RadioGroup.Item key={item} value={item}>
+                            <RadioGroup.ItemHiddenInput onBlur={field.onBlur} />
+                            <RadioGroup.ItemIndicator />
+                            <RadioGroup.ItemText>{item}</RadioGroup.ItemText>
+                          </RadioGroup.Item>
+                        ))}
+                      </HStack>
+                    </RadioGroup.Root>
+                  )} />
+              </Fieldset.Root>
+              {stId.field.value !== undefined &&
+                <Fieldset.Root>
+                  <Fieldset.Legend>Select related stimuli</Fieldset.Legend>
+                  <CheckboxGroup width="50%"
+                    value={related.field.value}
+                    onValueChange={(e) => {
+                      let old_rel = relation;
+                      old_rel[stId.field.value] = e;
+                      setRelation(old_rel)
+                      return related.field.onChange(e)
+                    }}
+                    name={related.field.name}
+                  >
+                    <Fieldset.Content>
+                      {poolId.field.value && !stimuli.loading && !stimuli.error && stimuli.value.map((item) => (
+                        <Checkbox.Root key={item} value={item}>
+                          <Checkbox.HiddenInput />
+                          <Checkbox.Control />
+                          <Checkbox.Label>{item}</Checkbox.Label>
+                        </Checkbox.Root>
+                      ))}
+                    </Fieldset.Content>
+                  </CheckboxGroup>
+                </Fieldset.Root>}
+            </Flex>}
+        </Container>
+        }
         <Button colorPalette="blue" type="submit"> <LuFlaskConical /> Create new experiment</Button>
       </Stack>
     </form>
